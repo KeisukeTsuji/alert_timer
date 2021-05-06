1c12472c2318ece67227051c35a696fc5c82db18
KeisukeTsuji <kei.iwmmd@gmail.com>
2021-05-06 18:56:37 +0900

    test追加

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,20 +1,31 @@
-import { shallowMount } from "@vue/test-utils";
+import { mount, shallowMount } from "@vue/test-utils";
+import Home from "@/pages/Home.vue";
 import ScheduleForm from "@/components/ScheduleForm.vue";
 import ScheduleList from "@/components/ScheduleList.vue";
+
+// alertでエラーを吐くのでnullに
+window.alert = jest.fn();
+window.alert = () => null;
 
 describe("ScheduleForm.vue", () => {
   test("propsを受け取れること", () => {
     const wrapper = shallowMount(ScheduleForm, {
       props: {
-        hour: "1",
-        minutes: "1",
         contents: "test",
+        selectTime: {
+          hour: "1",
+          minutes: "1",
+        },
         createSchedule: () => null,
       },
     });
     expect(wrapper.vm.$props.contents).toBe("test");
-    expect(wrapper.vm.$props.hour).toBe("1");
-    expect(wrapper.vm.$props.minutes).toBe("1");
+    expect(JSON.stringify(wrapper.vm.$props.selectTime)).toBe(
+      JSON.stringify({
+        hour: "1",
+        minutes: "1",
+      })
+    );
     expect(wrapper.vm.$props.createSchedule).toBeTruthy();
   });
 });
@@ -50,5 +61,17 @@ describe("ScheduleList.vue", () => {
       ])
     );
     expect(wrapper.vm.$props.deleteSchedule).toBeTruthy();
+  });
+});
+
+describe("Home.vue", () => {
+  test("登録ボタンクリック後にtableに登録内容が反映されるか", async () => {
+    const wrapper = mount(Home);
+    // console.log(wrapper.html());
+    wrapper.vm.contents = "test";
+    wrapper.vm.state.selectTime = { HH: "1", mm: "2" };
+    await wrapper.find(".create-button").trigger("click");
+    expect(wrapper.find(".td-1").text()).toMatch("test");
+    expect(wrapper.find(".td-2").text()).toMatch("01:02");
   });
 });
