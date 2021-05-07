60b64ffe618cbe7eb6afaa6ed41c02980b31dfd6
KeisukeTsuji <kei.iwmmd@gmail.com>
2021-05-07 22:13:45 +0900

    テストケース追加

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -65,22 +65,53 @@ describe("ScheduleList.vue", () => {
 });
 
 describe("Home.vue", () => {
-  test("登録ボタンクリック後にtableに登録内容が反映され、削除ボタンで該当項目を削除できるか", async () => {
+  test("登録時に予定内容が未入力だと登録されない", async () => {
     const wrapper = mount(Home);
+    wrapper.vm.contents = "";
+    wrapper.vm.state.selectTime = { HH: "1", mm: "2" };
+    await wrapper.find(".create-button").trigger("click");
+    console.log(wrapper.html());
+    expect(wrapper.find("#td_1_0").exists()).toBe(false);
+    expect(wrapper.find("#td_2_0").exists()).toBe(false);
+  });
+  test("登録時に開始時刻(hour)が未入力だと登録されない", async () => {
+    const wrapper = mount(Home);
+    wrapper.vm.contents = "test1";
+    wrapper.vm.state.selectTime = { HH: "", mm: "2" };
+    await wrapper.find(".create-button").trigger("click");
+    console.log(wrapper.html());
+    expect(wrapper.find("#td_1_0").exists()).toBe(false);
+    expect(wrapper.find("#td_2_0").exists()).toBe(false);
+  });
+  test("登録時に開始時刻(minutes)が未入力だと登録されない", async () => {
+    const wrapper = mount(Home);
+    wrapper.vm.contents = "test1";
+    wrapper.vm.state.selectTime = { HH: "1", mm: "" };
+    await wrapper.find(".create-button").trigger("click");
+    console.log(wrapper.html());
+    expect(wrapper.find("#td_1_0").exists()).toBe(false);
+    expect(wrapper.find("#td_2_0").exists()).toBe(false);
+  });
+  test("登録ボタンクリック後にtableに登録内容が反映される", async () => {
     // console.log(wrapper.html());
     // 1回目の入力
+    const wrapper = mount(Home);
     wrapper.vm.contents = "test1";
     wrapper.vm.state.selectTime = { HH: "1", mm: "2" };
     await wrapper.find(".create-button").trigger("click");
     expect(wrapper.find("#td_1_0").text()).toMatch("test1");
     expect(wrapper.find("#td_2_0").text()).toMatch("01:02");
-    // 2回目の入力
+  });
+  test("登録ボタンクリック後にtableに登録内容が反映される(2回目)", async () => {
+    const wrapper = mount(Home);
     wrapper.vm.contents = "test2";
     wrapper.vm.state.selectTime = { HH: "3", mm: "4" };
     await wrapper.find(".create-button").trigger("click");
     expect(wrapper.find("#td_1_0").text()).toMatch("test2");
     expect(wrapper.find("#td_2_0").text()).toMatch("03:04");
-    // test2の削除
+  });
+  test("削除ボタンで該当項目(test2)を削除できるか", async () => {
+    const wrapper = mount(Home);
     await wrapper.find("#delete_button_0").trigger("click");
     expect(wrapper.find("#td_1_0").text()).toMatch("test1");
     expect(wrapper.find("#td_2_0").text()).toMatch("01:02");
